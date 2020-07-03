eb4ffde99a8bb327733efc9c690bd51d083699ee
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-07-03 16:24:19 +0300

    Initial commit

---
+++ Decorators.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+
+def upper_case(old_foo):
+    def new_foo(*args, **kwargs):
+        result = old_foo(*args, **kwargs)
+        return result.upper()
+    return new_foo
+
+
+
+
+def logger(oldfoo):
+    date = datetime.now()
+
+    def newfoo(*args, **kwargs):
+        # nonlocal date
+        # nonlocal time
+        result = oldfoo(*args, **kwargs)
+        with open('logs/trace.log', 'a', encoding='utf-8') as fi:
+            fi.write(f'Вызвали функцию с именем "{oldfoo.__name__}"\n'
+                  f'c аргументами "{args}" и "{kwargs}"\n'
+                  f'Время старта: {date}\n'
+                  f'Возвращаемое значение: "{result}"\n'
+                     f'________________________________\n')
+            return result
+
+    return newfoo
+
+# @logger
+# def hello_world(a, b):
+#     return 'hello world'
+#
+# hello_world(1, 2)
